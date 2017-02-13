981b85f1edb8ab561ffcb9646856ec6d3684ae00
Scerelyn <Scerelyn@gmail.com>
2017-02-13 14:49:56 -0700

    Make road tile abstract

--- tstopsim/RoadTile.java
+++ tstopsim/RoadTile.java
@@ -3,13 +3,9 @@ package tstopsim;
 import java.awt.geom.*;
 import java.util.*;
 
-public class RoadTile implements Visualizable{
+public abstract class RoadTile implements Visualizable{
 
-    public static final int ROAD_DIMENTION = 100; //pixels side, length and width of the time
-    
-    @Override
-    public ArrayList<Rectangle2D.Double> assemble() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    public static final int ROAD_DIMENTION = 100; //pixels side, length and width of the tile
+    ArrayList<Rectangle2D.Double> parts;
     
 }
