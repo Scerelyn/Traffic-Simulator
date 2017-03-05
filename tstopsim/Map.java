205236afa33707af5c9a6430b9419d749ed0c6a9
Scerelyn <Scerelyn@gmail.com>
2017-03-04 23:05:11 -0700

    Add file parsing for building city maps

--- tstopsim/Map.java
+++ tstopsim/Map.java
@@ -2,6 +2,8 @@ package tstopsim;
 
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 import javax.swing.JComponent;
 
@@ -11,6 +13,10 @@ public class Map extends JComponent {
     
     public Map(){
         this.city = new RoadTile[][]{{new FourIntersectionRoadTile(0,0)}};
+    }
+    
+    public Map(File f){
+        this.city = parseFile(f);
     }
     
     @Override
@@ -91,5 +97,122 @@ public class Map extends JComponent {
     
     public void addVehicle(Vehicle v){
         this.cars.add(v);
+    }
+    
+    public RoadTile[][] parseFile(File f){
+        ArrayList<RoadTile> tempRow = new ArrayList<>();
+        RoadTile[][] tCity;
+        int rowCount = 0;
+        try{
+            Scanner in = new Scanner(f);
+            while(in.hasNextLine()){
+                in.nextLine();
+                rowCount++;
+            }
+        } catch(IOException e){
+            System.out.println("Missing file");
+        }
+        tCity = new RoadTile[rowCount][];
+        
+        try{
+            Scanner in = new Scanner(f);
+            int row = 0;
+            while (in.hasNextLine()) {
+                tempRow.clear();
+                String rowStr = in.nextLine().toLowerCase();
+                char[] rowArr = rowStr.toCharArray();
+
+                int pos = 0;
+                for (int i = 0; i < rowStr.length(); i++) {
+                    if (rowArr[i] != ' ') { //spaces ignored
+                        switch (rowArr[i]) {
+                            case 'b':
+                                tempRow.add(new NonDrivableRoadTile(pos, row));
+                                pos++;
+                                break;
+                            case 'i':
+                                tempRow.add(new FourIntersectionRoadTile(pos, row));
+                                pos++;
+                                break;
+                            case 'f':
+                                switch (rowArr[i + 1]) {
+                                    case 'n':
+                                        tempRow.add(new StraightRoadTile(pos, row, Direction.NORTH));
+                                        pos++;
+                                        break;
+                                    case 's':
+                                        tempRow.add(new StraightRoadTile(pos, row, Direction.SOUTH));
+                                        pos++;
+                                        break;
+                                    case 'e':
+                                        tempRow.add(new StraightRoadTile(pos, row, Direction.EAST));
+                                        pos++;
+                                        break;
+                                    case 'w':
+                                        tempRow.add(new StraightRoadTile(pos, row, Direction.WEST));
+                                        pos++;
+                                        break;
+                                }
+                                i++;
+                                break;
+                            case 't':
+                                switch (rowArr[i + 1]) {
+                                    case 'n':
+                                        tempRow.add(new TurnRoadTile(pos, row, Direction.NORTH));
+                                        pos++;
+                                        break;
+                                    case 's':
+                                        tempRow.add(new TurnRoadTile(pos, row, Direction.SOUTH));
+                                        pos++;
+                                        break;
+                                    case 'e':
+                                        tempRow.add(new TurnRoadTile(pos, row, Direction.EAST));
+                                        pos++;
+                                        break;
+                                    case 'w':
+                                        tempRow.add(new TurnRoadTile(pos, row, Direction.WEST));
+                                        pos++;
+                                        break;
+                                }
+                                i++;
+                                break;
+                            case 'h':
+                                switch (rowArr[i + 1]) {
+                                    case 'n':
+                                        tempRow.add(new TIntersectionRoadTile(pos, row, Direction.NORTH));
+                                        pos++;
+                                        break;
+                                    case 's':
+                                        tempRow.add(new TIntersectionRoadTile(pos, row, Direction.SOUTH));
+                                        pos++;
+                                        break;
+                                    case 'e':
+                                        tempRow.add(new TIntersectionRoadTile(pos, row, Direction.EAST));
+                                        pos++;
+                                        break;
+                                    case 'w':
+                                        tempRow.add(new TIntersectionRoadTile(pos, row, Direction.WEST));
+                                        pos++;
+                                        break;
+                                }
+                                i++;
+                                break;
+                            default:
+                                tempRow.add(new NonDrivableRoadTile(pos, row));
+                                pos++;
+                        }
+                    }
+                }
+                Object[] tArr = tempRow.toArray();
+                tCity[row] = new RoadTile[tArr.length];
+                for(int i = 0; i < tCity[row].length; i++){
+                    tCity[row][i] = (RoadTile)tArr[i];
+                }
+                row++;
+            }
+        } catch(IOException e){
+            System.out.println("Missing file");
+        }
+        return tCity;
     }
 }
